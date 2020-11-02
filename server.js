1244edf3f5ea3b6766aa56b851745bba35dfa462
Robin Renneke <rennekerobin@yahoo.com>
2020-11-01 21:51:03 -0600

    correct bug on serverjs

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgersController.js");
+const routes = require("./controllers/burgersController.js");
 
 app.use(routes);
 
