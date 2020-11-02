ce10c97eba2d049c28ac488694b1fdc3706c9f91
Robin Renneke <rennekerobin@yahoo.com>
2020-11-01 21:49:15 -0600

    correct connection typo

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 
 
 //now with mysqp we create a connection
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
